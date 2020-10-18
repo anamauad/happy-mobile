762f1a028271998d8c2dd5548cd855a868f52327
David Santos <contato.dvdsantos@gmail.com>
2020-10-18 18:24:12 -0300

    reupload

---
+++ src/routes.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+
+
+
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }} >
+            <Screen name="OrphanagesMap" component={OrphanagesMap} />
+            <Screen name="OrphanageDetails" component={OrphanageDetails} />
+            <Screen name="SelectMapPosition" component={SelectMapPosition} />
+            <Screen name="OrphanageData" component={OrphanageData} />
+
+
+            
+            </Navigator> 
+        </NavigationContainer>
+    );
+}
